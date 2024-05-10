8449af1c4dbabc34cc1bdba41c7fe91cb5129f0c
John Stachurski <john.stachurski@gmail.com>
2024-05-10 13:29:04 +1000

    misc

--- source_files/build_notebooks.py
+++ source_files/build_notebooks.py
@@ -1,7 +1,7 @@
 #! python
 import os
 import glob
-dirs = 'monday', 'tuesday', 'wednesday', 'thursday', 'friday'
+dirs = '1_monday', '2_tuesday', '3_wednesday', '4_thursday', '5_friday'
 
 print('Converting Myst files to notebooks.')
 for dir in dirs:
